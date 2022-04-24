ef78363a39777f3e215f56d178c349c1d9030b5c
BitGeekX <103322869+bitgeekx@users.noreply.github.com>
2022-04-25 02:41:17 +0800

    1

--- layout/common/head.jsx
+++ layout/common/head.jsx
@@ -175,6 +175,9 @@ module.exports = class extends Component {
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" async></script> : null}
 
             {followItVerificationCode ? <meta name="follow.it-verification-code" content={followItVerificationCode} /> : null}
+
+            {/* iconfont 补充图标 */}
+            <link rel="stylesheet" href="//at.alicdn.com/t/font_3355899_j3fm1sdmmcn.css" />
         </head>;
     }
 };
